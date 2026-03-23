67c2afb4c7173c978efa0ecc6677b078bc4bfa06
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2026-03-23 10:42:25 -0400

    init

---
+++ src/routes/(main)/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+import type { Snippet } from "svelte";
+
+const {
+	children,
+}: {
+	children: Snippet<[]>;
+} = $props();
+</script>
+
+<div class="wrap">
+	<div class="navBar">
+	    <div class='navBarInner'>
+					<h2>Games!</h2>
+		</div>
+	</div>
+	<div class="content">
+		{@render children?.()}
+	</div>
+</div>
+
+<style lang="scss">
+    .wrap {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+  import favicon from "$lib/assets/favicon.svg";
+  import "@fontsource-variable/geist/wght.css";
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head>
@@ -9,3 +10,20 @@
 </svelte:head>
 
 {@render children()}
+
+<style lang="scss">
+	:global(body) {
+		--background: #1f1f1f;
+		--background-alt: #2c2c2c;
+		--color: #fff;
+
+		background: var(--background);
+		color: var(--color);
+		margin: 0px;
+		padding: 0px;
+	}
+
+	:global(*) {
+		font-family: 'Geist Variable', sans-serif;
+	}
+</style>
